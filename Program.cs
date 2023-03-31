3b6af40886e1eddb203401fbe9462114596933a6
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-03-31 21:53:36 +0700

    SystemTime

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SystemTime;
+
+class Program {
+
+static void Main(string[] args) {
+DateTime localDate = DateTime.Now;
+System.Console.WriteLine("Datetime Now is :" + localDate);
+
+}}
+
+
